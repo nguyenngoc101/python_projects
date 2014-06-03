b1046ff6f8a96d52a116ebfdd797c68af00c16d0
ngocnv <nguyenngoc101@yahoo.com>
2014-06-03 23:00:03 +0700

    recursion practice

--- learn_python/function.py
+++ learn_python/function.py
@@ -28,8 +28,15 @@ def unflattern_dict(d, result=None):
 
 #>>> treemap(lambda x: x*x, [1, 2, [3, 4, [5]]])
 #[1, 4, [9, 16, [25]]]
-def tree_map(lambda_function, nested_list):
-    pass
+def tree_map(f, nested_list):
+    n = len(nested_list)
+    result = []
+    for x in range(0,n):
+        if isinstance(nested_list[x],list):
+            result.append(tree_map(f, nested_list[x]))
+        else:
+            result.append(f(nested_list[x]))
+    return result
 
 def tree_reverse(nested_list):
     n = len(nested_list)
@@ -59,13 +66,41 @@ def tree_reverse2(nested_list):
             result.append(nested_list[x])
     return result
 
+def unflatten(dictionary):
+    resultDict = dict()
+    for key, value in dictionary.iteritems():
+        parts = key.split(".")
+        d = resultDict
+        for part in parts[:-1]:
+            if part not in d:
+                d[part] = dict()
+            d = d[part]
+        d[parts[-1]] = value
+    return resultDict
 
-
-#d = {'a': 1, 'b': {'x': 2, 'y': {'d': 4, 'z': 5}}, 'c': 4}
+d = {'a': 1, 'b': {'x': 2, 'y': {'d': 4, 'z': 5}}, 'c': 4}
+d = {'a' : 'b', 'c.d' : 'z', 'c.e' : 1, 'c.d.e': 3}
 #print flatten_dict(d)
-#nested_list = [6,3, 2, 1]
+#nested_list = [1,[2,3]]
 #nested_list = [6, [[5, 4], 3], [2, 1]]
 #nested_list = [[1, 2], [3, [4, 5]], 6, [6,7]]
-nested_list = [1, 2, 3, 4, 5, 6]
-nested_list = [[3,5],6, [[5, 4], 3], [2, 1]]
-print tree_reverse2(nested_list)
+#nested_list = [1, 2, 3, 4, 5, 6]
+#nested_list = [[3,5],6, [[5, 4], 3], [2, 1]]
+#print tree_reverse2(nested_list)
+#print tree_map(lambda x: x*2, nested_list)
+print unflatten(d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
