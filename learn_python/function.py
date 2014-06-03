8fbe820ac9cec21d9dd378ae11a1be622a492c7f
ngocnv <nguyenngoc101@yahoo.com>
2014-06-03 18:26:12 +0700

    recursion practice

--- learn_python/function.py
+++ learn_python/function.py
@@ -26,20 +26,46 @@ def flatten_dict(d,result=None):
 def unflattern_dict(d, result=None):
     pass
 
+#>>> treemap(lambda x: x*x, [1, 2, [3, 4, [5]]])
+#[1, 4, [9, 16, [25]]]
 def tree_map(lambda_function, nested_list):
     pass
 
 def tree_reverse(nested_list):
     n = len(nested_list)
-    if n == 1:
-        return nested_list[:]
-    if n > 1:
-        return nested_list[n-1:] + tree_reverse(nested_list[:n-1])
+    if n-1 >= 0:
+        print n-1, nested_list[n-1]
+        tmp = tree_reverse(nested_list[:n-1])
+        if isinstance(nested_list[n-1], list):
+            if tmp:
+                return [tree_reverse(nested_list[n-1])]+(tmp)
+            else:
+                return tree_reverse(nested_list[n-1])
+        else:
+            if tmp:
+                return [nested_list[n-1]] + tmp
+            else:
+                return [nested_list[n-1]]
 
-d = {'a': 1, 'b': {'x': 2, 'y': {'d': 4, 'z': 5}}, 'c': 4}
-print flatten_dict(d)
+    return []
+
+def tree_reverse2(nested_list):
+    n = len(nested_list)
+    result = []
+    for x in range(n-1,-1,-1):
+        if isinstance(nested_list[x], list):
+            result.append(tree_reverse2(nested_list[x]))
+        else:
+            result.append(nested_list[x])
+    return result
+
+
+
+#d = {'a': 1, 'b': {'x': 2, 'y': {'d': 4, 'z': 5}}, 'c': 4}
+#print flatten_dict(d)
 #nested_list = [6,3, 2, 1]
 #nested_list = [6, [[5, 4], 3], [2, 1]]
-#[[1, 2], [3, [4, 5]], 6]
-#[6, [[5, 4], 3], [2, 1]]
-#print tree_reverse(nested_list)
+#nested_list = [[1, 2], [3, [4, 5]], 6, [6,7]]
+nested_list = [1, 2, 3, 4, 5, 6]
+nested_list = [[3,5],6, [[5, 4], 3], [2, 1]]
+print tree_reverse2(nested_list)
